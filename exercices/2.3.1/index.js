912898c7aef13568b904ce6120c4fb5c10c9b553
Ismo <ismet.ismet@student.vinci.be>
2023-10-13 14:24:41 +0200

    2.3.1 : event object & form

---
+++ exercices/2.3.1/index.js
@@ -0,0 +1,14 @@
+const formulaire = document.getElementById("formulaire");
+const souhaitInput = document.getElementById("souhaitInput");
+const resultat = document.getElementById("resultat");
+const resultText = document.getElementById("resultatText");
+
+formulaire.addEventListener("submit", function (e) {
+    e.preventDefault(); // Empêche le rechargement de la page
+
+    resultText.textContent = souhaitInput.value;
+        
+
+    formulaire.style.display = "none";
+    resultat.style.display = "block";
+});
